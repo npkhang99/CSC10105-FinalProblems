a72cbabc91192e0e4c177f779e18215e78f8736d
Phuc-Khang Nguyen <curoa99@gmail.com>
2020-08-11 17:29:53 +0700

    Fix bug bai-01.cpp

--- src/bai-01.cpp
+++ src/bai-01.cpp
@@ -1,17 +1,18 @@
+#include <cassert>
 #include <iostream>
 #include <algorithm>
-#include <iomanip>
 using namespace std;
 
 const int N = 100009;
 
 int n, s, a[N] = {};
 
+// hàm chỉ để debug với test có n <= 10^4
 int naive() {
     int count = 0;
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
-            if (a[i] + a[j] <= n) {
+            if (a[i] + a[j] <= s) {
                 count += 1;
             }
         }
@@ -25,27 +26,38 @@ int main() {
         cin >> a[i];
     }
 
-    int expected = naive();
-    cerr << expected << endl;
-
     sort(a, a + n);
-
-    for (int i = 0; i < n; i++) {
-        cerr << setw(2) << i << " \n"[i == n - 1];
-    }
-    for (int i = 0; i < n; i++) {
-        cerr << setw(2) << a[i] << " \n"[i == n - 1];
-    }
 
     long long count = 0;
     for (int i = 0; i < n; i++) {
         int need = s - a[i];
+        // tìm phần trong mảng a mà với mọi phần tử từ đó trở về trước (không tính nó) cộng với a[i] sẽ không vượt quá s đề cho
         int p = upper_bound(a, a + n, need) - a;
-        cerr << setw(2) << i << " " << setw(2) << a[i] << " " << setw(2) << need << " " << p << endl;
+        
+        // cứ cộng vào trước, tính sau
         count += p;
-        if (p >= i) {
+
+        /**
+         * nếu phần mình tìm lớn hơn hoặc bằng vị trí mình đang xét thì phải trừ 1 khỏi count
+         * vì nếu p > i thì sẽ bị tính a[i] + a[i] => đếm dư => phải trừ đi trường hợp này
+         * NOTE: vì mình tìm upper_bound nên sẽ không thể xảy ra trường hợp p == i nên cũng không cần xét >= chi cả
+         * nếu không có thì fail tại test:
+         * n = 3
+         * s = 6
+         * a = 3 3 3
+         */
+        if (p > i) {
             count -= 1;
         }
+    }
+
+    // vì mình sẽ đếm 2 lần trường hợp (i, j) và (j, i) nên phải cắt một nửa đi
+    count /= 2;
+
+    // nếu n <= 10000 thì so sánh với thuật ngây thơ O(n^2)
+    // nếu kết quả không đúng thì sẽ throw => Runtime error
+    if (n <= 10000) {
+        assert(count == naive());
     }
 
     cout << count << endl;
